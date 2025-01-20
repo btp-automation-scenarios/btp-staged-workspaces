ff23e108b62ebce1d1d200ab19b1d1fb1dae3420
Christian Lechner <lechnerc77@users.noreply.github.com>
2025-01-20 09:56:11 +0100

    refinement: GH Action and output (#6)
    Co-authored-by: Christian Lechner <22294087+lechnerc77@users.noreply.github.com>

--- infra/BTP/outputs.tf
+++ infra/BTP/outputs.tf
@@ -3,11 +3,6 @@ output "cf_api_url" {
   description = "The Cloud Foundry API URL"
 }
 
-output "cf_landscape_label" {
-  value       = btp_subaccount_environment_instance.cloudfoundry.landscape_label
-  description = "The Cloud Foundry landscape label"
-}
-
 output "cf_org_id" {
   value       = jsondecode(btp_subaccount_environment_instance.cloudfoundry.labels)["Org ID"]
   description = "The Cloud Foundry organization ID"
